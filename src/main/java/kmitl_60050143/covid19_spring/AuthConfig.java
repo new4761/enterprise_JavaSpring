99a8524c748d6514e991491c9a29c8f2059b7b40
new4761 <30498964+new4761@users.noreply.github.com>
2020-05-02 04:48:52 +0700

    fix security csrf pls fix it

--- src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
+++ src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
@@ -1,7 +1,7 @@
 package kmitl_60050143.covid19_spring;
 
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,14 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @Configuration
 public class AuthConfig extends WebSecurityConfigurerAdapter{
-    @Value("${security.enable-csrf}")
-    private boolean csrfEnabled;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        if(csrfEnabled){
-            http.csrf().disable();
-        }
+
+        http.csrf().disable();
         http.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
                 .requiresSecure()
