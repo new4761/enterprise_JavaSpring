bd9b048aef8f16fd076e10dc21c3e7d2b0125c4d
new4761 <30498964+new4761@users.noreply.github.com>
2020-05-02 04:13:21 +0700

    fix security csrf 4

--- src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
+++ src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
@@ -1,6 +1,7 @@
 package kmitl_60050143.covid19_spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,20 +15,20 @@ import java.util.Arrays;
 public class AuthConfig extends WebSecurityConfigurerAdapter{
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER") .and()
+                .withUser("admin").password("{noop}password").roles("ADMIN");
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
     }
 
 }
