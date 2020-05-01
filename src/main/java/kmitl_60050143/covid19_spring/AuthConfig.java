f227376d8f585889601af8d4744fbe648899911f
new4761 <30498964+new4761@users.noreply.github.com>
2020-05-02 04:23:48 +0700

    fix security csrf 5

--- src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
+++ src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
@@ -1,34 +1,20 @@
 package kmitl_60050143.covid19_spring;
 
-import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
+@Configuration
 public class AuthConfig extends WebSecurityConfigurerAdapter{
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER") .and()
-                .withUser("admin").password("{noop}password").roles("ADMIN");
-    }
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 
 }
