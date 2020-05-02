9f9d7c303456330aabf83164e4ea72e0bdd5a7e0
new4761 <30498964+new4761@users.noreply.github.com>
2020-05-02 16:05:14 +0700

    - - fix it6

--- src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
+++ src/main/java/kmitl_60050143/covid19_spring/AuthConfig.java
@@ -1,8 +1,6 @@
 package kmitl_60050143.covid19_spring;
 
 
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -10,22 +8,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
-@EnableWebSecurity
 @Configuration
-public class AuthConfig extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity
+public class AuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure (HttpSecurity http) throws Exception {
         http.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
                 .requiresSecure();
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated();
-        }
+        http.authorizeRequests()
+                .antMatchers("/").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        super.configure(http);
+    }
+
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure (WebSecurity web) throws Exception {
         web
                 .ignoring()
                 .antMatchers("/**");
